335b8f2a87052ab4ac74dfc592fe5b9e4bfb36aa
JevonHayter <jevonhayter@gmail.com>
2018-11-05 16:03:27 +0000

    added validations

--- app/models/lady.rb
+++ app/models/lady.rb
@@ -1,2 +1,5 @@
 class Lady < ApplicationRecord
+   validates :name, presence: true, length: { minimum: 2 }
+   validates :face, :chest, :abs, :butt, :legs, presence: true 
+   validates :face, :chest, :abs, :butt, :legs, numericality: true
 end
